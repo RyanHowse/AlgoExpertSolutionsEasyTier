20ff49d71a1c4af9932bf5e1b84fe0c6b68c73ba
RyanHowse <ryan.j.howse@gmail.com>
2021-05-24 16:04:15 -0400

    Create Question9EasyTier.py

---
+++ Question9EasyTier.py
@@ -0,0 +1,21 @@
+#Ryan Howse
+#5/24/2021
+#Depth-first Search of a tree using arrays
+#I originally was stumped by the array structure of the tree and had to look
+#at their c++ solution for help, but I've learned how to do it a bit cleaner now because of it,
+#and I was able to translate it over to Python myself
+
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        array.insert(len(array), self.name)
+		    for c in self.children:
+            c.depthFirstSearch(array)
+		    return array
