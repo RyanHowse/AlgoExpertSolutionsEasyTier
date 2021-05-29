6b9bd49b54188db7c77943f3aa49c542a717fb06
RyanHowse <ryan.j.howse@gmail.com>
2021-05-28 21:57:26 -0400

    Create Question18EasyTier.cpp

---
+++ Question18EasyTier.cpp
@@ -0,0 +1,25 @@
+//Ryan Howse
+//5/28/2021
+//Bubble Sort
+
+#include <vector>
+using namespace std;
+
+vector<int> bubbleSort(vector<int> array) {
+  //lets do a bubble sort
+	//the basic idea is that the bigger numbers
+	//slowly float to the top
+	//it does work
+	//but its O(n^2)
+	//so don't use it
+	for (int i = 0; i < array.size()-1; i++){
+		for (int j = 0; j < array.size()-1; j++){
+			if(array[j+1] < array[j]){
+				int temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+			}
+		}
+	}
+  return array;
+}
